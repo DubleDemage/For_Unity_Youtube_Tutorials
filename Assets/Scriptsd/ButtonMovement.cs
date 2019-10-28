c0bd92cfa8995d164cb3a3188fa2aeb1cc2dc106
Ahmad <cassdy-91@hotmail.com>
2019-10-28 13:39:49 +0000

    movement using buttons
    ettempted to move my character using buttons

---
+++ Assets/Scriptsd/ButtonMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMovement : MonoBehaviour
+{
+    public float playerSpeed;
+
+    Rigidbody rb;
+
+   
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void MoveRight()
+    {
+        transform.position += Vector3.right * 15.0f * Time.deltaTime;
+    }
+
+    public void MoveLeft()
+    {
+
+        transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+    }
+}
