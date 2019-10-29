0e805f81a586078cd1c9afacdf884337e8791dc6
Ahmad <cassdy-91@hotmail.com>
2019-10-29 13:18:31 +0000

    Switch scenes
    switching scenes in unity

---
+++ Assets/Scriptsd/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Scene2");
+        }
+    }
+}
